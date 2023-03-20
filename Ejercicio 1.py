7d1cc0e5c3a5407b8a00155c6665be9ea44aec2c
Lautaro Heiland <128431431+LautaroHei@users.noreply.github.com>
2023-03-20 19:54:53 -0300

    Add files via upload

---
+++ Ejercicio 1.py
@@ -0,0 +1,7 @@
+print("Ejercicio 1: CALCULAR DISTANCIA.")
+print("Ingrese la velocidad y el tiempo de la univdad móvil")
+V=float(input("Velocidad:"))
+T=int(input("Tiempo:"))
+D=V*T
+print("La distancia recorrida es de ...")
+print(D)
